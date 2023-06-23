ee43e839bc0ace6f066bc0ba06de9aeb3d19d8a6
bruno lino <brunolamegolino@gmail.com>
2023-06-23 21:37:14 +0000

    error handling adjustments

--- src/router.php
+++ src/router.php
@@ -6,11 +6,20 @@ require_once 'database.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if ($uri == '/hello') {
-    require 'hello.php';
-} elseif ($uri == '/product') {
-    require 'product/ProductController.php';
-} else {
-    header('HTTP/1.1 404 Not Found');
-    echo 'Page not found';
+try {
+    if ($uri == '/hello') {
+        require 'hello.php';
+    } elseif ($uri == '/product') {
+        require 'product/ProductController.php';
+    } else {
+        header('HTTP/1.1 404 Not Found');
+        echo 'Page not found';
+    }
+} catch (\Exception $e) {
+    if (getenv('DEBUG') == 'TRUE') {
+        throw $e;
+    } else {
+        header('HTTP/1.1 500 Internal Server Error');
+        echo 'Instability in the application. try again later.';
+    }
 }
